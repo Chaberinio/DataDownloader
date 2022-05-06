5df5d3ccdb5f28e9962ce57cbd02377f1453b48d
Chaberinio <chaber2222@o2.pl>
2022-05-06 14:43:14 +0200

    test

---
+++ DataDownloaders/RevolutDataDownloader.cs
@@ -0,0 +1,27 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDownloaders
+{
+    public class RevolutDataDownloader : IDataDownloader
+    {
+        public string GetAccounts()
+        {
+            return "";
+        }
+
+        public string GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetParameter(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataDownloaders/RevolutDataDownloaderMock.cs
@@ -0,0 +1,39 @@
+﻿using Core.Exceptions;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDownloaders
+{
+    public class RevolutDataDownloaderMock : IDataDownloader
+    {
+
+
+        string acc = "test";
+        string parameter = string.Empty;
+    
+
+
+
+        public string GetAccounts()
+        {
+            if (parameter=="AE")            
+              throw new AuthenticationException();
+            
+            return acc;
+        }
+
+        public string GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetParameter(string value)
+        {
+            parameter = value;
+        }
+    }
+}
